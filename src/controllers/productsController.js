8dc7dfde10fdfc771f46500dbba8e10b2b93532e
mostafa ahmed <mostafa.ahmed2509@gmail.com>
2023-07-08 07:01:19 +0300

    crud for products and products

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -1,31 +1,67 @@
 
-//gets all products
-exports.getProducts  =  (req,res) => {
+const productsModel = require('../models/productsModel');
 
-    
+
+exports.getProducts  =  async(req,res) => {
+
+    const result = await productsModel.find({}).catch(err => {
+        res.send(err.message);
+    });
+
+    res.send(result || "no products found");
 
 }
     
-exports.getProductById  =  (req,res) => {
+exports.getProductById  =  async(req,res) => {
     
+    const id = req.params.id;
     
-        
-}
-
-
-
-exports.createProduct  =  (req,res) => {
-
-   
-}
-
-exports.updateProduct =  (req,res) => {
+    try {
+        const result = await productsModel.findById({_id: id})
+        res.send(result || "product not found");
+    }
+    catch (err) {
+        res.send("wrong id format");
+    }
     
 }
 
-exports.deleteProduct  =  (req,res) => {
-    
+exports.createProduct  =  async(req,res) => {
 
+    const product = new productsModel(req.body);
+
+    const result = await product.save().catch(err => {
+        res.send(err.message);
+    });
+
+    res.send(result);
+
+}
+
+exports.updateProduct =  async(req,res) => {
+
+    const id = req.params.id;
+
+    try {
+        const result = await productsModel.updateOne({_id: id }, req.body)
+        res.send(result);
+    }
+    catch (err) {
+        res.send(err.message);
+    }
+}
+
+exports.deleteProduct  =  async(req,res) => {
+    
+    const id = req.params.id;
+
+    try {
+        const result = await productsModel.deleteOne({_id: id })
+        res.send(result);
+    }
+    catch (err) {
+        res.send(err.message);
+    }
        
 }
 
--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -1,34 +1,71 @@
 
-let yup = require('yup');
+const usersModel = require('../models/usersModel');
 
-//gets all products
-exports.getUsers  =  (req,res) => {
 
+exports.getUsers  =  async(req,res) => {
+
+    const result = await usersModel.find({}).catch(err => {
+        res.send(err.message);
+    });
+
+    res.send(result || "no users found");
 
 }
     
-
-exports.getUserById  =  (req,res) => {
+exports.getUserById  =  async(req,res) => {
     
-        
+    const id = req.params.id;
+    
+    try {
+        const result = await usersModel.findById({_id: id})
+        res.send(result || "user not found");
+    }
+    catch (err) {
+        res.send("wrong id format");
+    }
+    
 }
 
+exports.createUser  =  async(req,res) => {
 
-exports.createUser  =  (req,res) => {
+    const user = new usersModel(req.body);
 
+    const result = await user.save().catch(err => {
+        res.send(err.message);
+    });
+
+    res.send(result);
+
+}
+
+exports.updateUser =  async(req,res) => {
+
+    const id = req.params.id;
+
+    try {
+        const result = await usersModel.updateOne({_id: id }, req.body)
+        res.send(result);
+    }
+    catch (err) {
+        res.send(err.message);
+    }
+}
+
+exports.deleteUser  =  async(req,res) => {
+    
+    const id = req.params.id;
+
+    try {
+        const result = await usersModel.deleteOne({_id: id })
+        res.send(result);
+    }
+    catch (err) {
+        res.send(err.message);
+    }
        
 }
 
-exports.updateUser =  (req,res) => {
-    
-}
 
-
-exports.deleteUser  =  (req,res) => {
-    
-
-       
-}
 
 
 
